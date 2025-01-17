dd7c7c48ec7d09336d64d33e5c89e7f25bb11215
Muhammad Zain <zainarain2024@gmail.com>
2025-01-17 22:01:24 +0500

    Create header.js

---
+++ core/header.js
@@ -0,0 +1,22 @@
+const headers = {
+  Accept: "application/json, text/plain, */*",
+  "Accept-Encoding": "gzip, deflate, br",
+  "Accept-Language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+  "Content-Type": "application/json",
+  Origin: "https://game.kushempire.games",
+  referer: "https://game.kushempire.games/",
+  "Sec-Ch-Ua": '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+  host: "api.kushempire.games",
+  "Sec-Ch-Ua-Mobile": "?0",
+  "Sec-Ch-Ua-Platform": '"Windows"',
+  "Sec-Fetch-Dest": "empty",
+  "Sec-Fetch-Mode": "cors",
+  "Sec-Fetch-Site": "same-origin",
+  "Cache-Control": "no-cache",
+  Connection: "keep-alive",
+  "user-device": "Edge/131.0.0.0#Windows/10",
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+};
+
+module.exports = headers;
+
